c1e44d719906cef252b15152832e60bc47c68d28
RobertHaba <sedkipl98@gmail.com>
2022-04-25 12:24:20 +0200

    Connect Cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,32 @@
 module.exports = ({ env }) => ({
   // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
   email: {
     config: {
-      provider: 'sendgrid',
+      provider: "sendgrid",
       providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
+        apiKey: env("SENDGRID_API_KEY"),
+        localServer: {
+          maxage: 300000,
+        },
       },
       settings: {
-        defaultFrom: 'robertfrontend@gmail.com',
-        defaultReplyTo: 'robertfrontend@gmail.com',
-        testAddress: 'robertfrontend@gmail.com',
+        defaultFrom: "robertfrontend@gmail.com",
+        defaultReplyTo: "robertfrontend@gmail.com",
+        testAddress: "robertfrontend@gmail.com",
       },
     },
   },
